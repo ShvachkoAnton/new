b02c4526fc410a96210a9ca586e5be662987d612
zeratul314@gmail.com <zeratul314@gmail.com>
2021-06-23 21:10:37 +0300

    'bio'

--- my_awesome_project/users/models.py
+++ my_awesome_project/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, TextField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -9,6 +9,7 @@ class User(AbstractUser):
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    bio = TextField("Bio", blank=True)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
 
