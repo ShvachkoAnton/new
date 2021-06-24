3aae493b4eca5c0c3f4050a6a5990a61d7c9299c
zeratul314@gmail.com <zeratul314@gmail.com>
2021-06-24 18:49:38 +0300

    'cheese'

--- config/settings/base.py
+++ config/settings/base.py
@@ -78,6 +78,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "my_awesome_project.users.apps.UsersConfig",
+    "cheeses"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
